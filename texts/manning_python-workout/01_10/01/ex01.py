8fe5015cb81ca574bee1fd9d36b3fb8e62d6ffd8
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-01-08 13:03:48 -0500

    exercise 1 from python workout text

--- texts/manning_python-workout/01_10/01/ex01.py
+++ texts/manning_python-workout/01_10/01/ex01.py
@@ -1,13 +1,46 @@
 import random
+import sys
+
+
+def game_over(message="Game over."):
+	sys.exit(message)
+
 
 def guessing_game():
-  lower_bound = 0
-  upper_bound = 100
-  random_value = random.randint(lower_bound, upper_bound + 1)
-  tries = 3
+	lower_bound = 0
+	upper_bound = 100
+	random_value = random.randint(lower_bound, upper_bound + 1)
+	count = 0
+	max_tries = 15
 
-  print(f"You have {tries} tries to guess the correct value between {lower_bound} and {upper_bound}")
-  while true:
-    try{
-      guess = input(f"Guess a number between {lower_bound} and {upper_bound}.")
-    } catch
+	print(f"You have {max_tries} tries to guess the correct value between {lower_bound} and {upper_bound}")
+
+	while True:
+		try:
+			guess = input(f"Guess a number between {lower_bound} and {upper_bound}, or 'q' to quit. ")
+			if guess == 'q':
+				game_over()
+			elif not guess.isnumeric():
+				raise ValueError
+			else:
+				guess = int(guess)
+		except ValueError:
+			print(f"The value you entered ({guess}) is not a number.  Please enter a number.")
+			continue
+
+		if guess < random_value:
+			print("Too low")
+		elif guess > random_value:
+			print("Too high")
+		else:
+			print("You are correct!")
+			game_over()
+
+		count += 1
+		if count >= max_tries:
+			print(f"The value was {random_value}.  You are out of tries.")
+			game_over()
+
+
+if __name__ == "__main__":
+	guessing_game()
