e113482b9fd24114eccdfc4bdfccda022b51dc01
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-01-21 18:12:50 -0500

    some exercises from ch 01 of The Go Workshop

---
+++ texts/non-cert-prep/golang/the_golang_workshop/ch01/act_1.02.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var fname string = "Brian"
+var lname string = "Sigurdson"
+var age int = 0
+var peanutAlergy bool = false
+
+func main() {
+	fmt.Println(fname)
+	fmt.Println(lname)
+	fmt.Println(age)
+	fmt.Println(peanutAlergy)
+}
