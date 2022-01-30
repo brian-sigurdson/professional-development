f979e46eb122c67193b5ad995053d6067c6e3db5
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-01-30 16:18:19 -0500

    working on ch 2 of The Kubernetes Workshop

---
+++ texts/non-cert-prep/docker_kubernetes/the_kubernetes_workshop/ch02/act_2.01/build_push.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+docker_acct=sigurdsonbrian
+app_name=page-view
+version=v0.0.1
+full_name=${docker_acct}/${app_name}:${version}
+
+# check for existing image
+result=''
+result=$(docker image ls | grep -i ${docker_acct}/${app_name} | awk '{print $3}')
+if [[ ${result} != '' ]]; then
+    echo "Container exists with id: " ${result}
+    echo "Removing container:  docker rmi ${result}"
+    docker rmi ${result}
+fi
+# echo $result
+# echo $full_name
+
+# test if app_name already exists
+
+exit 1
+
+# complile
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go.exe build -o page-view
+
+# build the image
+echo "docker build -t ${full_name} ."
+docker build -t ${full_name} .
+
+# push it to docker hub
+echo "docker push docker build -t ${full_name} ."
+# docker build -t ${full_name} .
