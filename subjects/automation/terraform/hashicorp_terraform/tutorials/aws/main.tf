f433bea4b309c3d19fb7dbcb6394f2019904071d
Brian Sigurdson <sigurdson.brian@gmail.com>
2022-03-01 20:24:49 -0500

    hashicorp aws tutorial

---
+++ subjects/automation/terraform/hashicorp_terraform/tutorials/aws/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "app_server" {
+    # amazon linux
+    # ami           = "ami-0661cd3308ec33aaa" 
+    # ubuntu
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = var.instance_name
+    }
+}
