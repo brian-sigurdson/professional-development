865a4d03e97a9052f1784269412089f8af7c26c2
brian-sigurdson <sigurdson.brian@gmail.com>
2022-06-12 09:35:17 -0400

    starting over The Go Workshop

--- subjects/programming/golang/the_golang_workshop/ch01/ex_1.01.go
+++ subjects/programming/golang/the_golang_workshop/ch01/ex_1.01.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	initial_val := rand.Intn(5)
-	fmt.Println(initial_val)
+	// fmt.Println(initial_val)
 	initial_val += 1
 	stars := strings.Repeat("*", initial_val)
 	fmt.Println(stars)
