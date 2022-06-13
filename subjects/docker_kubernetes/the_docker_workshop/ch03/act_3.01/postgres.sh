09073427a32f89efb866920f081d892eb1fb17cf
brian-sigurdson <sigurdson.brian@gmail.com>
2022-06-13 18:19:18 -0400

    doing docker workshop again

--- subjects/docker_kubernetes/the_docker_workshop/ch03/act_3.01/postgres.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# this is what i ran to complete activity 1.01
-docker run -d --name postgres-db -e "POSTGRES_USER=panoramic" -e "POSTGRES_PASSWORD=trekking" postgres:12
--- subjects/docker_kubernetes/the_docker_workshop/ch05/ex_5.01/init/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-rm /data/index.html
-
-echo "<h1>Welcome from Docker Compose!</h1>" >> /data/index.html
-echo "<img src='http://bit.ly/moby-logo' />" >> /data/index.html
