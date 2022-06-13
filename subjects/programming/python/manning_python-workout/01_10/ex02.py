50ce4bd33460be23020c96d75765f29247be4c88
brian-sigurdson <sigurdson.brian@gmail.com>
2022-06-12 20:45:30 -0400

    python workshop

--- subjects/programming/python/manning_python-workout/01_10/ex02.py
+++ subjects/programming/python/manning_python-workout/01_10/ex02.py
@@ -1,1 +1,15 @@
-# nothing yet
+from doctest import OutputChecker
+
+
+def mysum(*numbers):
+  output = 0
+
+  for number in numbers:
+    output += number
+
+  print(f"sum = {output}")
+
+
+mysum(1,2,3)
+
+mysum(*[2,2,3])
---
+++ subjects/programming/python/manning_python-workout/test.py
@@ -0,0 +1,4 @@
+name = "brian"
+
+print(f"Hello {name:#<20}")
+print(f"Hello {name:#>20}")
