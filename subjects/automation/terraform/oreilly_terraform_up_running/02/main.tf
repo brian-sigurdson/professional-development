191f93e9be1f2c614cf71f3a5421ec973bb43ce5
brian-sigurdson <sigurdson.brian@gmail.com>
2022-06-18 16:47:15 -0400

    brikman ch02

--- subjects/automation/terraform/oreilly_terraform_up_running/02/main.tf
+++ subjects/automation/terraform/oreilly_terraform_up_running/02/main.tf
@@ -2,24 +2,9 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-02d1e544b84bf7502"
-  instance_type = "t2.micro"
-
-  security_groups = [aws_security_group.instance.name]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "Hello World" > index.html
-    nohup busybox httpd -f -p "${var.server_port}" &
-    EOF
-
-  tags = {
-    "Name"    = "terraform-example"
-    "Chapter" = "2"
-    "Eample"  = "single-server"
-  }
-}
+###################################################################################################
+# supplemental resources
+###################################################################################################
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -29,16 +14,132 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     to_port     = var.server_port
   }
-
 }
 
-variable "server_port" {
-  description = "The port the server will use for http requests"
-  type        = number
-  default     = 8080
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # allow inbound http
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the webserver"
+data "aws_vpc" "default" {
+  default = true
 }
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+###################################################################################################
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  max_size = 10
+  min_size = 2
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-02f3416038bdb17fb" # ubuntu should have busybox installed by default
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo $(hostname -I) > index.html
+    nohup busybox httpd -f -p "${var.server_port}" &
+    EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+###################################################################################################
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # by default return a simple 404 page
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+###################################################################################################
