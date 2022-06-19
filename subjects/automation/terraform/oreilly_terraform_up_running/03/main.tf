7322e5f526c0a93ca72e3463dca664db6d42ef0c
brian-sigurdson <sigurdson.brian@gmail.com>
2022-06-18 21:36:38 -0400

    brikman

--- subjects/automation/terraform/oreilly_terraform_up_running/03/main.tf
+++ subjects/automation/terraform/oreilly_terraform_up_running/03/main.tf
@@ -1,13 +1,14 @@
 locals {
-  region      = "us-east-2"
-  bucket_name = "bks-name-${local.region}-tf-brikman-state"
+  region              = "us-east-2"
+  s3_bucket_name      = "bks-name-${local.region}-tf-brikman-state"
+  dynamodb_table_name = "tf-up-running-locks"
 }
 provider "aws" {
   region = local.region
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = local.bucket_name
+  bucket = local.s3_bucket_name
 
   # prevent accidental deletion
   lifecycle {
@@ -15,16 +16,44 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 
   # enable versioning to keep a full history of state file revisions
-  versioning {
-    enabled = true
-  }
+  # versioning {
+  #   enabled = true
+  # }
 
   # enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  # server_side_encryption_configuration {
+  #   rule {
+  #     apply_server_side_encryption_by_default {
+  #       sse_algorithm = "AES256"
+  #     }
+  #   }
+  # }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = local.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
