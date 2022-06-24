d150761dd3722a624f3e1dba7b02484d7ba9cd80
brian-sigurdson <sigurdson.brian@gmail.com>
2022-06-23 20:58:05 -0400

    brikman working through ch 05

--- subjects/automation/terraform/oreilly_terraform_up_running/global/iam/outputs.tf
+++ subjects/automation/terraform/oreilly_terraform_up_running/global/iam/outputs.tf
@@ -24,3 +24,11 @@ output "all_arns2" {
   value       = values(aws_iam_user.example)[*].arn
   description = "The ARNs for all users"
 }
+
+output "upper_names" {
+  value = [for name in var.user_names : upper(name)]
+}
+
+output "upper_names_less_5" {
+  value = [for name in var.user_names : upper(name) if length(name) < 5]
+}
