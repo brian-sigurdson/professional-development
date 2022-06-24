79e83038a286a7302190b1d9b6d2ecf430e9e064
brian-sigurdson <sigurdson.brian@gmail.com>
2022-06-24 09:00:10 -0400

    brikman working through ch 05

--- subjects/automation/terraform/oreilly_terraform_up_running/global/iam/outputs.tf
+++ subjects/automation/terraform/oreilly_terraform_up_running/global/iam/outputs.tf
@@ -32,3 +32,15 @@ output "upper_names" {
 output "upper_names_less_5" {
   value = [for name in var.user_names : upper(name) if length(name) < 5]
 }
+
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+
+output "upper_roles_1" {
+  value = { for name in var.user_names : upper(name) => name }
+}
+
+output "upper_roles_2" {
+  value = { for name, role in var.hero_thousand_faces : upper(name) => upper(role) }
+}
