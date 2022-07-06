2cd04da5ae52ce2b4c101b1fd1a5595670113160
brian-sigurdson <sigurdson.brian@gmail.com>
2022-07-06 08:52:54 -0400

    brikman ch 7 work

--- subjects/automation/terraform/oreilly_terraform_up_running/07/alb_example_test.go
+++ subjects/automation/terraform/oreilly_terraform_up_running/07/alb_example_test.go
@@ -1,11 +1,27 @@
 package test 
 import (
+	"fmt"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/gruntwork-io/terratest/modules/http-helper"
 	"testing"
 ) 
 
 func TestAlbExample(t *testing.T) {
 	opts := &terraform.Options{
-		TerraformDir: "../examples/alb"
+		TerraformDir: "../examples/alb",
 	}
+
+	// terraform.Init(t, opts)
+	// terraform.Apply(t, opts)
+	terraform.InitAndApply(t, opt)
+
+	// get the url of the alb
+	albDnsName := terraform.OutputRequired(t, opts, "alb_dns_name")
+	url := fmt.Sprintf("http://&s", albDnsName)
+
+	// test that the alb's default action is working and returns a 404
+	expectedStatus := 404
+	expectedBody := "404: page not found"
+
+	http_helper.HttpGetWithValidation(t, url, expectedStatus, expectedBody)
 }
