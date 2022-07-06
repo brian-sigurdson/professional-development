4aa53f33e2d344a729d5a4853fd535c8fafc366a
brian-sigurdson <sigurdson.brian@gmail.com>
2022-07-06 08:59:50 -0400

    brikman ch 7 work

--- subjects/automation/terraform/oreilly_terraform_up_running/07/alb_example_test.go
+++ subjects/automation/terraform/oreilly_terraform_up_running/07/alb_example_test.go
@@ -11,8 +11,13 @@ func TestAlbExample(t *testing.T) {
 		TerraformDir: "../examples/alb",
 	}
 
+	// clean up everything at the end of the test
+	defer terraform.Destroy(t, opts)
+
+	// no need to call these individually
 	// terraform.Init(t, opts)
 	// terraform.Apply(t, opts)
+	// deploy the example
 	terraform.InitAndApply(t, opt)
 
 	// get the url of the alb
@@ -22,6 +27,11 @@ func TestAlbExample(t *testing.T) {
 	// test that the alb's default action is working and returns a 404
 	expectedStatus := 404
 	expectedBody := "404: page not found"
+	// don't want to call this immediately
+	// http_helper.HttpGetWithValidation(t, url, expectedStatus, expectedBody)
 
-	http_helper.HttpGetWithValidation(t, url, expectedStatus, expectedBody)
+	maxRetries := 10
+	timeBetweenRetries := 10 * time.Second
+
+	http_helper.HttpGetWithRetry(t, url, expectedStatus, expectedBody, maxRetries, timeBetweenRetries)
 }
