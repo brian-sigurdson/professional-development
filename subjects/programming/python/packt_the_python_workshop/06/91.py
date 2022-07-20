9a39d7ce9f60edff5e1a2d33bc6e0b860a221216
brian-sigurdson <sigurdson.brian@gmail.com>
2022-07-20 08:39:38 -0400

    pws ch 6

---
+++ subjects/programming/python/packt_the_python_workshop/06/91.py
@@ -0,0 +1,14 @@
+import subprocess
+import os
+
+result = subprocess.run(
+    ["env"],
+    capture_output=True,
+    text=True,
+    env={**os.environ, "SERVER": "OTHER_SERVER"}
+)
+
+# print(result.stdout)
+myenv = result.stdout.split("\n")
+for val in myenv:
+    print(val)
