e2cc3b5c08ac006d239c92c97c8a725acd4d493b
brian-sigurdson <sigurdson.brian@gmail.com>
2022-07-21 08:10:01 -0400

    pws

---
+++ subjects/programming/python/packt_the_python_workshop/06/95.py
@@ -0,0 +1,16 @@
+import urllib.request
+import collections
+
+url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt'
+response = urllib.request.urlopen(url)
+words = response.read().decode().split()
+print(len(words))
+
+word_counter = collections.Counter(words)
+for word, count in word_counter.most_common(5):
+    print(word, " - ", count)
+
+print("QUESTION", " - ", word_counter["QUESTION"])
+print("CIRCUMFLEX", " - ", word_counter["CIRCUMFLEX"])
+print("DIGIT", " - ", word_counter["DIGIT"])
+print("PYTHON", " - ", word_counter["PYTHON"])
