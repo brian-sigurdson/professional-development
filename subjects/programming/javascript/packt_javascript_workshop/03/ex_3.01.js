96c33f7e35f0d97ff18eed53fe85e5567c3aad7b
brian-sigurdson <sigurdson.brian@gmail.com>
2022-07-27 20:35:00 -0400

    start jws

--- subjects/programming/javascript/packt_javascript_workshop/03/ex_3.01.js
+++
@@ -1,6 +0,0 @@
-// seeing if i can do some of the browser based exercises with node
-
-console.log(2 + 6);
-console.log("2" + "6");
-console.log("2" == "6");
-console.log("2" === "6");
--- subjects/programming/javascript/packt_javascript_workshop/03/ex_3.02.js
+++
@@ -1,5 +0,0 @@
-let firstName;
-console.log("typeof firstName: " + typeof firstName);
-
-firstName = "Albert";
-console.log("typeof firstName: " + typeof firstName);
