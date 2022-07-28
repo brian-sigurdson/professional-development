76c3abb8a92566fb524d39d3fe2c339e9d039298
brian-sigurdson <sigurdson.brian@gmail.com>
2022-07-28 11:23:21 -0400

    gws

---
+++ subjects/programming/golang/the_golang_workshop/ch01/1.12.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var count int
+	fmt.Print("count : %#v ", count)
+
+	// var discount float64
+	// fmt.Println("discount : %#v", discount)
+
+	// var debug bool
+	// fmt.Println("debug : %#v", debug)
+
+	// var message string
+	// fmt.Println("message : %#v", message)
+
+	// var emails []string
+	// fmt.Println("emails : %#v", emails)
+
+	var startTime time.Time 
+	fmt.Println("startTime : %#v", startTime)
+}
