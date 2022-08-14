99168192a7bd9b3bae0aae02b91ada4e92f6f4b9
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-14 14:21:18 -0400

    jsbp

---
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/06/6.01.js
@@ -0,0 +1,8 @@
+function myfunc(param1, param2) {
+    return param1 + param2
+}
+
+let val1 = 1;
+let val2 = 2;
+
+console.log(myfunc(val1, val2));
---
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/06/6.02.js
@@ -0,0 +1,5 @@
+myarr = ["Alaine", "Bob", "Chris"];
+
+val = Math.trunc(Math.random()* myarr.length);
+
+console.log("Hi " + myarr[val]);
