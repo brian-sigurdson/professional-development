b18a1b662f0ea2c452dc6a571ad393a11d0495ad
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-19 09:12:04 -0400

    jsbp 9.03

---
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/09/9.03/app.js
@@ -0,0 +1,8 @@
+let el1 = document.querySelector("div");
+console.log(el1);
+
+el1.textContent = "bks";
+el1.classList.add("red");
+el1.id = "tester";
+let theBody = window.document.body;
+theBody.style.backgroundColor = "red";
