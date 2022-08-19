a35da681311f1510318c7604425c0ab068d4abad
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-19 08:46:31 -0400

    jsbp 8.06

---
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/08/8.06.js
@@ -0,0 +1,8 @@
+console.log(Math.PI);
+console.log(Math.ceil(5.7654321));
+console.log(Math.floor(5.7654321));
+console.log(Math.round(5.7654321));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10 + 1);
+console.log(Math.random()*100 + 1);
