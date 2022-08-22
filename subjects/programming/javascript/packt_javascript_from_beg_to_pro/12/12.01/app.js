17f3712c3e4122432aef2c260f1bd9737e6dfab8
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-22 14:18:06 -0400

    jsbp 12.01

---
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/12/12.01/app.js
@@ -0,0 +1,13 @@
+// paused here.  I'll come back and finish later, once I'm focused on frontend stuff.
+
+let sText = document.getElementById("sText").value;
+let rText = document.getElementById("rText").value;
+let button = document.getElementById("button1").addEventListener("click", update);
+
+
+function update() { 
+    // console.log(sText);
+    // console.log(rText);
+    sText.replace(/rText/g, )
+}
+
