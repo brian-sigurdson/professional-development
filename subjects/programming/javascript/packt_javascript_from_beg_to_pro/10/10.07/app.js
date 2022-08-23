9320a445b42b3b77346dff03ae9f117e73ede011
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-23 14:30:36 -0400

    starting back on ch 10 again

--- subjects/programming/javascript/packt_javascript_from_beg_to_pro/10/10.07/app.js
+++
@@ -1,3 +0,0 @@
-function reveal(el){
-    console.dir(el);
-}
--- subjects/programming/javascript/packt_javascript_from_beg_to_pro/10/10.09/app.js
+++
@@ -1,8 +0,0 @@
-const btns = document.querySelectorAll("button");
-
-btns.forEach((btn)=>{
-    function output(){
-        console.log(this.textContent);
-    }
-    btn.addEventListener("click",output);
-});
