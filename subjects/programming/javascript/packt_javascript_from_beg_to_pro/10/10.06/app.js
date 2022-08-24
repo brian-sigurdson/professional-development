b005ebe9d5ef9160154e46b6142ea8872f5e944a
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-24 09:19:20 -0400

    jsbp 10.06

---
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/10/10.06/app.js
@@ -0,0 +1,11 @@
+let divs = document.querySelectorAll(".example");
+
+console.log(divs);
+
+for (let i = 0; i < divs.length; i++) {
+    console.log(divs[i]);
+}
+
+for (let i = 0; i < divs.length; i++) {
+    console.log(divs[i].textContent);
+}
