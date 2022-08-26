847d7ea45f9f277e5175faebb4dce4c3ee05dd6c
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-26 09:52:39 -0400

    jsbp 10.08

--- subjects/programming/javascript/packt_javascript_from_beg_to_pro/10/10.08/app.js
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/10/10.08/app.js
@@ -5,7 +5,33 @@ document.body.appendChild(table);
 
 function generateTableHeader(table) {
     let thead = table.createTHead();
+    let row = thead.insertRow();
+
+    let thIndex = document.createElement("th");
+    let indexText = document.createTextNode("#");
+    thIndex.appendChild(indexText);
+    row.appendChild(thIndex);
+
+    let thNames = document.createElement("th");
+    let namesText = document.createTextNode("Names");
+    thNames.appendChild(namesText);
+    row.appendChild(thNames);
+}
+
+function generateTableBody(table, data) {
+    let cnt = 1;
+    data.forEach(element => {
+        let row = table.insertRow();
+        let cellIndex = row.insertCell();
+        let cellIndexValue = document.createTextNode(cnt++);
+        cellIndex.appendChild(cellIndexValue);
+        
+        let cellName = row.insertCell();
+        let cellNameValue = document.createTextNode(element);
+        cellName.appendChild(cellNameValue);
+    });
 }
 
 generateTableHeader(table);
+generateTableBody(table, names);
 console.log(table);
