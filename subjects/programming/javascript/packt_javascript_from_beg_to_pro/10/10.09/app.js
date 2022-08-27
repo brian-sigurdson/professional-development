d9137bbef87c8c3ff61936d83a3645b53baf6c97
brian-sigurdson <sigurdson.brian@gmail.com>
2022-08-27 14:19:25 -0400

    jsbp 10.09

---
+++ subjects/programming/javascript/packt_javascript_from_beg_to_pro/10/10.09/app.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach( (btn) => {
+    function output() {
+        console.log(this.textContent);
+    }
+    btn.addEventListener("click", output);
+})
