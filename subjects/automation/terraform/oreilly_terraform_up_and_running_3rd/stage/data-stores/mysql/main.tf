05e9890786fe2a52e09b30c8cc437de37a267764
brian-sigurdson <sigurdson.brian@gmail.com>
2022-11-28 09:01:54 -0500

    added database

---
+++ subjects/automation/terraform/oreilly_terraform_up_and_running_3rd/stage/data-stores/mysql/main.tf
@@ -0,0 +1,25 @@
+terraform {
+    backend "s3" {
+        bucket = "name-bks-terraform-up-and-running-3rd-state"
+        key = "stage/data-stores/mysql/terraform.tfstate"
+        region = "us-east-1"
+    
+        dynamodb_table = "name-bks-terraform-up-and-running-3rd-locks"
+        encrypt = true 
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_db_instance" "example" {
+    identifer_prefix = "tfuar"
+    engine = "mysql"
+    allocated_storage = 10
+    instance_class = "db.t2.micro"
+    skip_final_snapshot = true
+    db_name = "example_db"
+    username = var.db_admin_username
+    password = var.db_admin_password
+}
---
+++ subjects/automation/terraform/oreilly_terraform_up_and_running_3rd/stage/data-stores/mysql/variables.tf
@@ -0,0 +1,11 @@
+variable "db_admin_username" {
+  type        = string
+  description = "The admin username"
+  sensitive = true 
+}
+
+variable "db_admin_password" {
+  type        = string
+  description = "The admin password"
+  sensitive = true 
+}
