0fe2f84133164669eb0c3147a0c5a96635c73531
brian-sigurdson <sigurdson.brian@gmail.com>
2022-12-04 13:54:19 -0500

    update

--- subjects/serverless/udemy_aws-lambda-serverless-arch-bootcamp/21/hello-world/app.js
+++ subjects/serverless/udemy_aws-lambda-serverless-arch-bootcamp/21/hello-world/app.js
@@ -1,1 +1,53 @@
-console.log("hello world");
+// console.log("hello world");
+
+// var x = "hello";
+// var y = "world";
+// console.log(x + y);
+
+// var z;
+// console.log(z);
+
+// function arr_func(item) {
+//     console.log(item);
+// }
+// var arr = [10, 20, 30];
+// arr.forEach(arr_func);
+// arr.forEach(item => {
+//     console.log(item);
+// })
+
+// function doStuff(data, callback) {
+//     callback("done");
+// }
+// doStuff(true, (result)=>console.log(result));
+
+function doStuff(data){
+    return new Promise(
+        (resolve, reject)=>{
+            let successMessage = {
+                status: 'success',
+                message: 'All is well!'
+            };
+    
+            let errorMessage = {
+                status: 'error',
+                message: 'Oops, there was an error!'
+            };
+
+            if(typeof data === 'boolean' && data === true){
+                resolve(successMessage);
+            } else {
+                reject(errorMessage);
+            }
+        }
+    )
+}
+
+doStuff(false).then(
+    (successMessage) => {
+        console.log(successMessage);
+    },    
+    (errorMessage) => {
+        console.log(errorMessage);
+    }
+);
