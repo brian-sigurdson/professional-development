39b5b2e2b0655d12034671e5e215f42b1e7561b1
brian-sigurdson <sigurdson.brian@gmail.com>
2022-12-07 20:28:08 -0500

    update

---
+++ subjects/serverless/udemy_aws-lambda-serverless-arch-bootcamp/02/greetme/indes.js
@@ -0,0 +1,28 @@
+const moment = require("moment/moment");
+
+const greeting = {
+    "en": "hello",
+    "fr": "bonjour",
+    "hi": "namaste"
+}
+
+exports.handler = async (event) => {
+
+    // node 8 example, doesn't work with node 18
+    // i won't mess with for now
+    let name = event.pathParameters.name;
+    let {lang, ...info} = event.queryStringParameters;
+
+    let message = `${greeting[lang] ? greeting[lang] : greeting['en'] } ${name}`
+
+    let response = {
+        message: message,
+        info: info,
+        timestamp: moment().unix()
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    }
+}
