3031f9d2893826e9a1320d969e7168047b3746fd
brian-sigurdson <sigurdson.brian@gmail.com>
2022-12-13 10:26:28 -0500

    updates

--- subjects/serverless/udemy_aws-lambda-serverless-arch-bootcamp/02/greetme/index.js
+++ subjects/serverless/udemy_aws-lambda-serverless-arch-bootcamp/02/greetme/index.js
@@ -23,6 +23,9 @@ export async function handler(event) {
 
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": '*'
+        },
         body: JSON.stringify(response)
     }
 }
