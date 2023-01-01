dae6828a5a3fed6bab54eb026ca3a44e34982244
brian-sigurdson <sigurdson.brian@gmail.com>
2022-12-31 19:31:43 -0500

    updates

--- subjects/automation/terraform/oreilly_terraform_up_and_running_3rd/live/global/iam/variables.tf
+++ subjects/automation/terraform/oreilly_terraform_up_and_running_3rd/live/global/iam/variables.tf
@@ -4,3 +4,13 @@ variable "user_names" {
   default     = ["neo", "trinity", "morpheus"]
   # default     = ["neo", "morpheus"]
 }
+
+variable "hero_thousand_faces" {
+  description = "map"
+  type = map(string)
+  default = {
+    neo = "hero"
+    trinity = "love interest"
+    morpheus = "mentor"
+  }
+}
