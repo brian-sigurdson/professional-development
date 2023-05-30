a45a9a4ac7c7b520356275d634b427bdb92080c4
brian-sigurdson <sigurdson.brian@gmail.com>
2023-05-30 14:59:41 -0400

    module 4

--- subjects/automation/aws/cdk/PS_aws-infra-with-TypeScript-getting-started/bin/ps_aws-infra-with-type_script-getting-started.ts
+++ subjects/automation/aws/cdk/PS_aws-infra-with-TypeScript-getting-started/bin/ps_aws-infra-with-type_script-getting-started.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { PsAwsInfraWithTypeScriptGettingStartedStack } from '../lib/ps_aws-infra-with-type_script-getting-started-stack';
 
 const app = new cdk.App();
