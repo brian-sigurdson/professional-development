399e6d30c551ffb1d2af089808bbc3e340aa5aa8
brian-sigurdson <sigurdson.brian@gmail.com>
2023-07-13 19:47:47 -0400

    cdk workshop

--- subjects/automation/aws/cdk/aws-docs/cdk-workshop/lib/cdk-workshop-stack.ts
+++ subjects/automation/aws/cdk/aws-docs/cdk-workshop/lib/cdk-workshop-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { HitCounter } from './hitcounter';
-import { TableViewer } from 'cdk-dynamo-table-viewer';
+// import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 
 export class CdkWorkshopStack extends cdk.Stack {
