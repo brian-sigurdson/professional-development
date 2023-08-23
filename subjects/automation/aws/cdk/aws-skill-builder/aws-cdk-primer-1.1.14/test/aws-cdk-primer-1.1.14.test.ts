79fde0b3aefe459105621fa7ed6634b3f685b731
brian-sigurdson <sigurdson.brian@gmail.com>
2023-08-23 15:23:11 -0400

    skill-builder

---
+++ subjects/automation/aws/cdk/aws-skill-builder/aws-cdk-primer-1.1.14/test/aws-cdk-primer-1.1.14.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AwsCdkPrimer1114 from '../lib/aws-cdk-primer-1.1.14-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-primer-1.1.14-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkPrimer1114.AwsCdkPrimer1114Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
