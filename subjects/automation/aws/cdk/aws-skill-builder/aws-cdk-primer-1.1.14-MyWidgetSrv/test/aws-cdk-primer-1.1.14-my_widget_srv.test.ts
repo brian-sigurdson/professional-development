5bb7d3898d359164858f81824081d43d4fba473c
brian-sigurdson <sigurdson.brian@gmail.com>
2023-08-25 14:39:41 -0400

    cdk skill-builder

---
+++ subjects/automation/aws/cdk/aws-skill-builder/aws-cdk-primer-1.1.14-MyWidgetSrv/test/aws-cdk-primer-1.1.14-my_widget_srv.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AwsCdkPrimer1114MyWidgetSrv from '../lib/aws-cdk-primer-1.1.14-my_widget_srv-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-primer-1.1.14-my_widget_srv-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkPrimer1114MyWidgetSrv.AwsCdkPrimer1114MyWidgetSrvStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
---
+++ subjects/automation/aws/cdk/aws-skill-builder/aws-cdk-primer-1.1.14-vpc/test/aws-cdk-primer-1.1.14-vpc.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AwsCdkPrimer1114Vpc from '../lib/aws-cdk-primer-1.1.14-vpc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-primer-1.1.14-vpc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkPrimer1114Vpc.AwsCdkPrimer1114VpcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
