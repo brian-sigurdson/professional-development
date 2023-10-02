ced846cb65952346af514f1d8b94a877fd77dffd
brian-sigurdson <sigurdson.brian@gmail.com>
2023-10-02 10:39:23 -0700

    acg cf beg course

--- subjects/automation/aws/cdk/udemy_aws-ts-masterclass-cdkv2-serverless-react/cdk-starter/bin/cdk-starter.ts
+++ subjects/automation/aws/cdk/udemy_aws-ts-masterclass-cdkv2-serverless-react/cdk-starter/bin/cdk-starter.ts
@@ -3,7 +3,18 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStarterStack } from '../lib/cdk-starter-stack';
 import { PhotosStack } from '../lib/PhotoStack';
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './tagger';
 
 const app = new cdk.App();
-new CdkStarterStack(app, 'CdkStarterStack', {});
-new PhotosStack(app, 'PhotosStack', {});
+// independent of other stacks
+// new CdkStarterStack(app, 'CdkStarterStack', {});
+const photosStack = new PhotosStack(app, 'PhotosStack', {});
+
+// needs PhotosStack deployed first
+new PhotosHandlerStack(app, 'PhotosHandlerStack', {
+    targetBucketArn: photosStack.photosBucketArn
+});
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
