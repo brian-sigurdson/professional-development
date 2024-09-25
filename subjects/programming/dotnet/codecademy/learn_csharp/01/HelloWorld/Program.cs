7913ec1664b760e5551b54364ae6e645b91bd98e
brian-sigurdson <sigurdson.brian@gmail.com>
2024-09-25 00:09:30 -0400

    dotnet

---
+++ subjects/programming/dotnet/codecademy/learn_csharp/01/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // your code here
+        Console.WriteLine("Hello, world!");
+    }
+}
