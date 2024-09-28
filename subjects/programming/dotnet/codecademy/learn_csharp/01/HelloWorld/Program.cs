93aa69a6d65f2d956e1bcb152342a9f76637d54f
brian-sigurdson <sigurdson.brian@gmail.com>
2024-09-28 15:43:49 -0400

    dotnet

--- subjects/programming/dotnet/codecademy/learn_csharp/01/HelloWorld/Program.cs
+++ subjects/programming/dotnet/codecademy/learn_csharp/01/HelloWorld/Program.cs
@@ -6,7 +6,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        // your code here
-        Console.WriteLine("Hello, world!");
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
     }
 }
